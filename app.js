3a8244e372ae78ae626245ead454201f7fac8282
Raghul1995 <rahulmohan997@gmail.com>
2021-07-02 15:02:53 +0200

    updated the data.json

--- app.js
+++ app.js
@@ -11,16 +11,14 @@ var RecipeRouter = require("./routes/recipe");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
