60b7bd487dc4251776575ba57f5b39b95304e3cb
Raghul1995 <rahulmohan997@gmail.com>
2021-07-06 22:51:40 +0200

    api changes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var RecipeRouter = require("./routes/recipe");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const RecipeRouter = require("./routes/recipe");
 
-var app = express();
+const app = express();
 
 
 app.use(cors());
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const utils = require("../utils")
+const data = require("../data.json")
 
+router.get('/', (req, res, next) => {
 
-router.get('/',  async(req, res, next) => {  
-
-  const dishes = await utils.getDishes()
+  //const dishes = await utils.getDishes()
+  const dishes = data
   res.json(dishes)
 
-      });
-router.get('/random',  async(req, res, next) => {  
+});
+router.get('/random', async (req, res, next) => {
 
   const randomDish = await utils.getRandomDish()
   res.json(randomDish)
 
-      });
-router.get('/unique/:id',  async(req, res, next) => {  
+});
+router.get('/unique/:id', async (req, res, next) => {
   const randomDish = await utils.getOneDish(req.params.id)
   res.json(randomDish)
 
-      });
+});
 
 module.exports = router;
